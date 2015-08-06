4f171c38c493f3eb9b00d6cb42a80cb0444f759e
Владимир <vkharseev@gmail.com>
2015-08-06 18:00:47 +0300

    Меню -dicval

--- app/views/templates/site/footer.blade.php
+++ app/views/templates/site/footer.blade.php
@@ -6,10 +6,18 @@
 <div class="sub-footer">
     <div class="copyright">
         <p>ООО «ЮНИЛЕВЕР РУСЬ». ЮРИДИЧЕСКИЙ И ПОЧТОВЫЙ АДРЕС: 123022, Г. МОСКВА, УЛ. СЕРГЕЯ МАКЕЕВА, Д. 13. </p>
+
         <p>ЭЛЕКТРОННАЯ ПОЧТА: <a href="mailto:COMMUNICATIONS.RUSSIA@UNILEVER.COM">COMMUNICATIONS.RUSSIA@UNILEVER.COM</a>
         </p>
     </div>
     {{ Menu::placement('footer_menu') }}
+    <div>
+        <span> Рассказать друзьям
+            <a href="#" class="soc ok"></a>
+            <a href="#" class="soc vk"></a>
+            <a href="#" class="soc fb"></a>
+        </span>
+    </div>
 </div>
 <div class="footer-decor-left"></div>
 <div class="footer-decor-right"></div>
