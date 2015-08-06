95aa9f9e234e53398f84c555a3a2750f39c4ba78
Владимир <vkharseev@gmail.com>
2015-08-06 17:40:38 +0300

    Шаблоны страниц

--- app/views/templates/site/header.blade.php
+++ app/views/templates/site/header.blade.php
@@ -11,3 +11,29 @@
 
     @endif
 </header>
+
+<div class="block header">
+    <div class="partners-block">
+        <div class="partner-traveller"></div>
+        <div class="partner-lingualeo"></div>
+    </div>
+    <div class="content">
+    @if (Request::is('/'))
+        <div class="logo-main"></div>
+    @else
+        <a href="/" class="logo-main"></a>
+    @endif
+    </div>
+    <div class="icons-block">
+        <a href="#" class="ico-account"></a>
+        <div href="#" class="ico-burger"></div>
+    </div>
+    <div class="right-menu-holder">
+        {{ Menu::placement('main_menu') }}
+    </div>
+</div>
+<div class="block menu-holder">
+    <div class="content">
+        {{ Menu::placement('center_menu') }}
+    </div>
+</div>
---
+++ app/views/templates/site/popups.blade.php
@@ -0,0 +1,17 @@
+<?
+/**
+ * TEMPLATE_IS_NOT_SETTABLE
+ */
+?>
+<div class="block second-code">
+    <div class="second-code-hack"></div>
+    <form name="promo-code-2" class="promo-code promo-code-2">
+        <h3>Есть еще один промо-код?</h3>
+
+        <div class="fields-holder">
+            <input type="text" for="promo-code-2" autocomplete="off">
+            <button disabled="disabled">Отправить</button>
+            <a href="#">Отмена</a>
+        </div>
+    </form>
+</div>
