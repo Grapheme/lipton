54f072796565e933b6b7894a8791884cab434958
Владимир <vkharseev@gmail.com>
2015-08-07 14:15:29 +0300

    Регистрация участника используя Email

---
+++ app/views/templates/participant/dashboard.blade.php
@@ -0,0 +1,17 @@
+<?
+/**
+ * TEMPLATE_IS_NOT_SETTABLE
+ */
+?>
+<?php
+$profile = Accounts::where('id', Auth::user()->id)->with('ulogin')->first();
+?>
+@extends(Helper::layout())
+@section('style')
+@stop
+@section('page_class')  @stop
+@section('content')
+    
+@stop
+@section('scripts')
+@stop
