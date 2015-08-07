ca277c4125387b34ef17298105e1e1aad1f58e3a
Владимир <vkharseev@gmail.com>
2015-08-07 10:56:31 +0300

    Условия использования

---
+++ app/modules/application/models/Accounts.php
@@ -0,0 +1,14 @@
+<?php
+
+class Accounts extends \User {
+
+    public static $rules = array(
+        'name' => 'required', 'email' => 'required|email', 'location' => 'required', 'age' => 'required',
+        'phone' => 'required', 'agree1' => 'required', 'agree2' => 'required'
+    );
+
+    public function ulogin() {
+
+        return $this->hasOne('Ulogin', 'user_id', 'id');
+    }
+}
--- app/views/templates/site/auth.blade.php
+++ app/views/templates/site/auth.blade.php
@@ -1,8 +1,8 @@
 <?
 /**
-* TITLE: Авторизация
-* AVAILABLE_ONLY_IN_ADVANCED_MODE
-*/
+ * TITLE: Авторизация
+ * AVAILABLE_ONLY_IN_ADVANCED_MODE
+ */
 ?>
 @extends(Helper::layout())
 @section('style')
@@ -13,17 +13,21 @@
             <div class="content">
                 <div class="participate-block">
                     <h2>{{ $page->seo->h1 }}</h2>
+
                     <div class="forms-holder">
                         @include(Helper::layout('forms.auth'))
                         <div class="devider"></div>
                         <div class="login-block">
                             <h3>Нет учетной записи?</h3>
                             <a href="{{ pageurl('registration') }}" class="login">Зарегистрироваться</a>
+
                             <h3>Войти через соцсеть</h3>
-                            <div class="soc-holder">
-                                <a class="soc fb"></a>
-                                <a class="soc vk"></a>
-                                <a class="soc ok"></a>
+
+                            <div class="soc-holder" id="uLogin_490f3e3f" data-uloginid="490f3e3f"
+                                 data-ulogin="mobilebuttons=0;display=buttons;fields=first_name,last_name,email,city,photo,photo_big;redirect_uri={{ URL::route('signin.ulogin') }}">
+                                <a data-uloginbutton="facebook" class="soc fb"></a>
+                                <a data-uloginbutton="vkontakte" class="soc vk"></a>
+                                <a data-uloginbutton="odnoklassniki" class="soc ok"></a>
                             </div>
                         </div>
                     </div>
