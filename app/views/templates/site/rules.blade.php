26ed5d5809c05f3e07fe16c311f847d0c15284cb
Владимир <vkharseev@gmail.com>
2015-08-13 14:33:51 +0300

    Шаблон Правила и условия использования

--- app/views/templates/site/rules.blade.php
+++ app/views/templates/site/rules.blade.php
@@ -1,14 +1,42 @@
 <?
 /**
-* TITLE: Правила акции
-* AVAILABLE_ONLY_IN_ADVANCED_MODE
-*/
+ * TITLE: Правила акции
+ * AVAILABLE_ONLY_IN_ADVANCED_MODE
+ */
 ?>
 @extends(Helper::layout())
 @section('style')
 @stop
 @section('content')
+    <div class="main-content sub-page">
+        <div class="block">
+            <div class="content">
+                <h2>{{ $page->seo->h1 }}</h2>
 
+                <div class="rules-row">
+                    <div class="rule-item rule-1">
+                        <div class="rule-img"></div>
+                        <h3>Купи новинку чая <a href="#">Lipton Discovery</a></h3>
+                    </div>
+                    <div class="rule-item rule-2">
+                        <div class="rule-img"></div>
+                        <h3>Зарегистрируй код из-под крышки на&nbsp;сайте</h3>
+                    </div>
+                    <div class="rule-item rule-3">
+                        <div class="rule-img"></div>
+                        <h3>Гарантированно Получи приз от&nbsp;Lingualeo</h3>
+                    </div>
+                    <div class="rule-item rule-4">
+                        <div class="rule-img"></div>
+                        <h3>Возможность отправиться в&nbsp;путешествие</h3>
+                    </div>
+                </div>
+                <div class="rules-text-block">
+                    {{ $page->block('content') }}
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
 @section('scripts')
 @stop
