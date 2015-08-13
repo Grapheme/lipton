7fac12535f180c6d4448566a35a4e1bec75138a2
Kirill Fadeev <fk@realitygroup.ru>
2015-08-13 17:53:59 +0300

    fix

--- app/views/templates/site/index.blade.php
+++ app/views/templates/site/index.blade.php
@@ -35,7 +35,7 @@ endif;
                         <div class="step-number">{{ $index + 1 }}</div>
                         <h2>{{ $step['title'] }}</h2>
                         <p>{{ $step['desc'] }}</p>
-                        @if ($index == 1)
+                        @if ($index == 2)
                             @include(Helper::layout('forms.first-promo-code'))
                         @endif
                     </div>
