ddee1f32f763584a891d23580b425db23513a1f8
Kirill Fadeev <fk@realitygroup.ru>
2015-08-19 17:31:54 +0300

    fix

--- public/theme/src/scripts/main.js
+++ public/theme/src/scripts/main.js
@@ -325,9 +325,9 @@ $( document ).ready(function() {
       }
     },   
     submitHandler: function(form) {
-      $('form.registration button').addClass('loading');
       $('form.registration button').prepend('<i class="fa fa-circle-o-notch fa-spin"></i>');
-      $('erros-message-block').remove();
+      $('form.registration button').addClass('loading');
+      $('.erros-message-block').remove();
       var options = { 
         success: function(data){
           if(data.redirectURL) {
