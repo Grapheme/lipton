cd4dce4de107c2aae79e66b9c3a51f70c8c4dc19
Владимир <vkharseev@gmail.com>
2015-08-19 17:43:17 +0300

    fix dashboard

--- app/views/templates/participant/dashboard.blade.php
+++ app/views/templates/participant/dashboard.blade.php
@@ -38,15 +38,17 @@ $prizes = (new ApiController())->get_prizes($post);
                 <div class="profile">
                     <div class="profile-head">
                         <div class="profile-info">
-                        @if(!empty($profile->photo) && File::exists(public_path($profile->photo)))
-                            <div style="background-image: url({{ asset($profile->photo) }});" class="avatar"></div>
-                        @elseif(!empty($profile->ulogin) && !empty($profile->ulogin->photo_big))
-                            <div style="background-image: url({{ asset($profile->ulogin->photo_big) }});" class="avatar"></div>
-                        @else
-                            <img src="{{ asset(Config::get('site.theme_path').'/images/avatar_default.png') }}" alt="{{ $profile->name }}"
-                                  class="avatar">
-                        @endif
+                            @if(!empty($profile->photo) && File::exists(public_path($profile->photo)))
+                                <div style="background-image: url({{ asset($profile->photo) }});" class="avatar"></div>
+                            @elseif(!empty($profile->ulogin) && !empty($profile->ulogin->photo_big))
+                                <div style="background-image: url({{ asset($profile->ulogin->photo_big) }});"
+                                     class="avatar"></div>
+                            @else
+                                <div style="background-image: url({{ asset(Config::get('site.theme_path').'/images/avatar_default.png') }});"
+                                     class="avatar"></div>
+                            @endif
                             <h3>{{ $profile->name }} {{ $profile->surname }}</h3>
+
                             <p>{{ $bdate->diffInYears($now).' '.Lang::choice('год|года|лет', $bdate->diffInYears($now)) }}{{ !empty($profile->city) ? ', '.$profile->city : '' }}</p>
                             <a href="{{ URL::route('profile.edit') }}">Редактировать профиль</a>
                         </div>
@@ -59,9 +61,12 @@ $prizes = (new ApiController())->get_prizes($post);
                             @if(count($profile->codes) == 2 && isset($profile->writing->writing) && !empty($profile->writing->writing))
                                 <a href="javascript:void(0);">Конкурс рассказов</a>
                                 @if($profile->writing->status == 2)
-                                <div class="moderation neutral">Ваш рассказ на модерации<div class="bullet"></div></div>
+                                    <div class="moderation neutral">Ваш рассказ на модерации
+                                        <div class="bullet"></div>
+                                    </div>
                                 @elseif($profile->writing->status == 1)
-                                <a class="watch" target="_blank" href="{{ URL::route('show.participant.writing', $profile->writing->id.'-'.BaseController::stringTranslite(Auth::user()->name.'-'.Auth::user()->surname)) }}">Смотреть</a>
+                                    <a class="watch" target="_blank"
+                                       href="{{ URL::route('show.participant.writing', $profile->writing->id.'-'.BaseController::stringTranslite(Auth::user()->name.'-'.Auth::user()->surname)) }}">Смотреть</a>
                                 @endif
                             @else
                                 <a class="disabled-button" href="javascript:void(0);">Конкурс рассказов</a>
@@ -76,30 +81,30 @@ $prizes = (new ApiController())->get_prizes($post);
                             <div class="ico leo"></div>
                             <p>Курс английского для путешественников</p>
                             @if(count($profile->codes) >= 1)
-                            <a class="disabled-button">Получен</a>
+                                <a class="disabled-button">Получен</a>
                             @endif
                         </div>
                         <div class="prize">
                             <div class="ico spec"></div>
                             <p>Cпецкурс<br>на выбор</p>
                             @if(count($profile->codes) == 2)
-                            <a class="disabled-button">Получен</a>
+                                <a class="disabled-button">Получен</a>
                             @endif
                         </div>
                         <div class="prize">
                             <div class="ico ngt"></div>
                             <p>Путешествие с national geographic traveler</p>
-                        @if(count($profile->codes) == 2)
-                            @if(isset($profile->writing->status) && $profile->writing->status == 1)
-                            <a class="disabled-button">Одобрен</a>
-                            @elseif(isset($profile->writing->status) && $profile->writing->status == 2)
-                            <a class="disabled-button">Модерация</a>
-                            @elseif(isset($profile->writing->status) && $profile->writing->status == 3)
-                            <a href="{{ URL::route('profile.tell-story') }}">Изменить</a>
-                            @else
-                            <a href="{{ URL::route('profile.tell-story') }}">Написать</a>
+                            @if(count($profile->codes) == 2)
+                                @if(isset($profile->writing->status) && $profile->writing->status == 1)
+                                    <a class="disabled-button">Одобрен</a>
+                                @elseif(isset($profile->writing->status) && $profile->writing->status == 2)
+                                    <a class="disabled-button">Модерация</a>
+                                @elseif(isset($profile->writing->status) && $profile->writing->status == 3)
+                                    <a href="{{ URL::route('profile.tell-story') }}">Изменить</a>
+                                @else
+                                    <a href="{{ URL::route('profile.tell-story') }}">Написать</a>
+                                @endif
                             @endif
-                        @endif
                         </div>
                     </div>
                 </div>
@@ -108,10 +113,10 @@ $prizes = (new ApiController())->get_prizes($post);
     </div>
 @stop
 @section('scripts')
-@if($prizes === -1)
-    <?php Auth::logout(); ?>
-    <script type="application/javascript">
-        window.location.href = '{{ pageurl('auth') }}';
-    </script>
-@endif
+    @if($prizes === -1)
+        <?php Auth::logout(); ?>
+        <script type="application/javascript">
+            window.location.href = '{{ pageurl('auth') }}';
+        </script>
+    @endif
 @stop
