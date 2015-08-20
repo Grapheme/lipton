9bb2fd6eaca015ed8c5439829b8c9567ef32cfcb
Kirill Fadeev <fk@realitygroup.ru>
2015-08-20 15:59:43 +0300

    forms

--- public/theme/build/scripts/main.concat.js
+++ public/theme/build/scripts/main.concat.js
@@ -1494,8 +1494,16 @@ $( document ).ready(function() {
     $('.learn-more-popup-holder').fadeOut();
   });
 
-  var smsAjaxAdress = $('form.sms-chesk .js-sms-again').attr('href');
-  $('form.sms-chesk').ajaxSend(smsAjaxAdress);
+
+  $('.js-sms-again').click(function(e){
+    e.preventDefault();
+    var smsAjaxAdress = $('form.sms-chesk .js-sms-again').attr('href');
+    $.post( "smsAjaxAdress", function(data) {
+      if (true !== data.status) {
+        alert('error accured');
+      }
+    });
+  })
 
 // WONDERFULL MASKS //
 
@@ -1985,8 +1993,10 @@ $( document ).ready(function() {
 
       if(userPhone != userPhoneNew) {
         var smsAjaxAdress = $('form.sms-chesk .js-sms-again').attr('href');
-        $.post( "smsAjaxAdress", function() {
-          alert( "success" );
+        $.post( "smsAjaxAdress", function(data) {
+          if (true !== data.status) {
+            alert('error accured');
+          }
         });
         $('.sms-wrapper').fadeIn();
         return false;
