2c9c4a3dea8580ccab839e9867ebc67e3a4786f4
Kirill Fadeev <fk@realitygroup.ru>
2015-08-20 11:49:57 +0300

    fix

--- app/views/templates/site/rules.blade.php
+++ app/views/templates/site/rules.blade.php
@@ -16,19 +16,19 @@
                 <div class="rules-row">
                     <div class="rule-item rule-1">
                         <div class="rule-img"></div>
-                        <h3>Купи новинку чая в промо упаковке <a href="#">Lipton Discovery</a><sup>*</sup></h3>
+                        <h3>Купите новинку чая в промо упаковке <a href="#">Lipton Discovery</a><sup>*</sup></h3>
                     </div>
                     <div class="rule-item rule-2">
                         <div class="rule-img"></div>
-                        <h3>Зарегистрируй код из-под крышки на&nbsp;сайте</h3>
+                        <h3>Зарегистрируйте код из-под крышки на&nbsp;сайте</h3>
                     </div>
                     <div class="rule-item rule-3">
                         <div class="rule-img"></div>
-                        <h3>Гарантированно Получи приз от&nbsp;Lingualeo</h3>
+                        <h3>Гарантированно получите приз от&nbsp;Lingualeo</h3>
                     </div>
                     <div class="rule-item rule-4">
                         <div class="rule-img"></div>
-                        <h3>Получи возможность отправиться в&nbsp;путешествие</h3>
+                        <h3>Получите возможность отправиться в&nbsp;путешествие</h3>
                     </div>
                 </div>
                 <div class="rules-text-block">
