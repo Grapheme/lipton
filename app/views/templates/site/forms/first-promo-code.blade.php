08880b08274c0f292f39aeee242bfbfc42d411e0
Владимир <vkharseev@gmail.com>
2015-08-21 17:35:34 +0300

    Потребитель. Получение 2-го приза

--- app/views/templates/site/forms/first-promo-code.blade.php
+++ app/views/templates/site/forms/first-promo-code.blade.php
@@ -5,13 +5,13 @@
 ?>
 
 @if(Auth::check() && Auth::user()->group_id == 4)
-{{ Form::open(array('route'=>'promo.first.register', 'id'=>'promo-code-form','name'=>'promo-code', 'class'=>'promo-code', 'data-user-auth'=>'authorized')) }}
-{{ Form::text('promoCode1', NULL, array('for'=>'promo-code', 'autocomplete'=>'off', 'class'=>'promoCode1')) }}
-{{ Form::button('Отправить',array('type'=>'submit')) }}
-{{ Form::close() }}
+    {{ Form::open(array('route'=>'promo.first.register', 'id'=>'promo-code-form','name'=>'promo-code', 'class'=>'promo-code', 'data-user-auth'=>'authorized')) }}
+    {{ Form::text('promoCode1', NULL, array('for'=>'promo-code', 'autocomplete'=>'off', 'class'=>'promoCode1')) }}
+    {{ Form::button('Отправить',array('type'=>'submit')) }}
+    {{ Form::close() }}
 @else
-{{ Form::open(array('route'=>'mainpage', 'id'=>'promo-code-form','name'=>'promo-code', 'class'=>'promo-code')) }}
-{{ Form::text('promoCode1', NULL, array('for'=>'promo-code', 'autocomplete'=>'off', 'class'=>'promoCode1')) }}
-{{ Form::button('Отправить',array('type'=>'submit', 'data-redirect-authorization'=>pageurl('auth'))) }}
-{{ Form::close() }}
+    {{ Form::open(array('route'=>'mainpage', 'id'=>'promo-code-form','name'=>'promo-code', 'class'=>'promo-code')) }}
+    {{ Form::text('promoCode1', NULL, array('for'=>'promo-code', 'autocomplete'=>'off', 'class'=>'promoCode1')) }}
+    {{ Form::button('Отправить',array('type'=>'submit', 'data-redirect-authorization'=>pageurl('auth'))) }}
+    {{ Form::close() }}
 @endif
