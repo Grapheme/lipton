d39b4f70c6d3039e2695dbfbdde589e7c90ff3a8
Kirill Fadeev <fk@realitygroup.ru>
2015-08-21 12:12:15 +0300

    fix

--- app/views/templates/site/forms/auth.blade.php
+++ app/views/templates/site/forms/auth.blade.php
@@ -4,7 +4,6 @@
  */
 ?>
 {{ Form::open(array('route'=>'auth.participant','class'=>'registration','name'=>'registration-form')) }}
-<a class="popup-close-cross" href=""></a>
 {{ Form::hidden('promo-code', isset($_COOKIE['firstCodeCookie']) ? str_replace(' ', '', $_COOKIE['firstCodeCookie']) : '' ) }}
 {{ Form::text('login', NULL, array('for'=>'registration-form','placeholder'=>'Электронная почта','autocomplete'=>'off')) }}
 {{ Form::password('password',array('for'=>'registration-form','placeholder'=>'Пароль','autocomplete'=>'off')) }}
