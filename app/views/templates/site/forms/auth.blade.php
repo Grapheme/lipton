5f55c7f1da5f66a5424316b4851a28b89edabb6b
Kirill Fadeev <fk@realitygroup.ru>
2015-08-21 15:43:18 +0300

    fix

--- app/views/templates/site/forms/auth.blade.php
+++ app/views/templates/site/forms/auth.blade.php
@@ -7,6 +7,6 @@
 {{ Form::hidden('promo-code', isset($_COOKIE['firstCodeCookie']) ? str_replace(' ', '', $_COOKIE['firstCodeCookie']) : '' ) }}
 {{ Form::text('login', NULL, array('for'=>'registration-form','placeholder'=>'Электронная почта','autocomplete'=>'off')) }}
 {{ Form::password('password',array('for'=>'registration-form','placeholder'=>'Пароль','autocomplete'=>'off')) }}
-<a class="password-recovery" href="#">Забыли пароль?</a>
+<a class="password-recovery" href="javascript:void(0);">Забыли пароль?</a>
 {{ Form::button('Войти',array('type'=>'submit')) }}
 {{ Form::close() }}
