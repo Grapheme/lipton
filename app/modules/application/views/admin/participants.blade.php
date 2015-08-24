54b8c73f10fe2b08821a2753a727852c8505e5ef
Владимир <vkharseev@gmail.com>
2015-08-24 11:13:14 +0300

    Получение списка победителей

--- app/modules/application/views/admin/participants.blade.php
+++ app/modules/application/views/admin/participants.blade.php
@@ -14,8 +14,8 @@ $now = Carbon::now();
                 <tr>
                     <th class="col-lg-1 text-center">ID</th>
                     <th class="col-lg-2 text-center">Фото и видео</th>
-                    <th class="col-lg-6 text-center">Данные пользователя</th>
-                    <th class="col-lg-3 text-center"></th>
+                    <th class="col-lg-4 text-center">Данные пользователя</th>
+                    <th class="col-lg-5 text-center"></th>
                 </tr>
                 </thead>
                 <tbody>
@@ -33,27 +33,18 @@ $now = Carbon::now();
                         <td>{{ ($index+1)+($sub_index*20) }}</td>
                         <td>
                         @if(!empty($user['photo']) && File::exists(public_path($user['photo'])))
-                            <img src="{{ asset($user['photo']) }}"
+                            <img height="100px" src="{{ asset($user['photo']) }}"
                                  alt="{{ $user['name'] }}" class="{{ $user['name'] }}">
                         @elseif(!empty($user['ulogin']) && !empty($user['ulogin']['photo_big']))
-                            <img src="{{ $user['ulogin']['photo_big'] }}" alt="{{ $user['name'] }}"
+                            <img height="100px" src="{{ $user['ulogin']['photo_big'] }}" alt="{{ $user['name'] }}"
                                  class="{{ $user['name'] }}">
                         @elseif(!empty($user['photo']))
-                            <img src="{{ $user['photo'] }}" alt="{{ $user['name'] }}"
+                            <img height="100px" src="{{ $user['photo'] }}" alt="{{ $user['name'] }}"
                                  class="{{ $user['name'] }}">
                         @else
-                            <img src="{{ asset(Config::get('site.theme_path').'/images/avatar_default.png') }}" alt="{{ $user['name'] }}"
+                            <img height="100px" src="{{ asset(Config::get('site.theme_path').'/images/avatar_default.png') }}" alt="{{ $user['name'] }}"
                                  class="{{ $user['name'] }}">
                         @endif
-                            <div style="margin-top: 50px">
-                                @if(count($user['writing']) && !empty($user['writing']['writing']))
-                                    <p><a target="_blank" href="{{ URL::route('show.participant.writing', $user['writing']['id'].'-'.BaseController::stringTranslite($user['name'].'-'.$user['surname'])) }}">Смотреть рассказ</a></p>
-                                @elseif(count($user['writing']) && empty($user['writing']['writing']))
-                                    <p>Рассказ пустой</p>
-                                @else
-                                    <p>Рассказ отсутсвует</p>
-                                @endif
-                            </div>
                         </td>
                         <td>
                             <p>
@@ -65,28 +56,38 @@ $now = Carbon::now();
                                 <i class="fa fa-envelope-o"></i> {{ HTML::mailto($user['email'], $user['email']) }}<br/>
                                 <i class="fa fa-fw fa-mobile-phone"></i>{{ $user['phone'] }}
                             </p>
-                        @if(count($user['writing']) && !empty($user['writing']['writing']))
-                            <hr style="margin-bottom: 5px; margin-top: 5px;">
-                            @if($user['writing']['status'] == 2)
-                            <span class="label label-primary">Ожидает модерации</span><br><br>
-                            <a href="{{ URL::route('moderator.participants.status', array($user['id'], $user['writing']['id'], 1)) }}" class="btn btn-success btn-xs js-confirm">Одобрить рассказ</a>
-                            <a href="{{ URL::route('moderator.participants.status', array($user['id'], $user['writing']['id'], 3)) }}" class="btn btn-danger btn-xs js-confirm">Отклонить рассказ</a>
-                            @elseif($user['writing']['status'] == 1)
-                            <span class="label label-success">Рассказ одобрен</span><br><br>
-                            <a href="{{ URL::route('moderator.participants.status', array($user['id'], $user['writing']['id'], 3)) }}" class="btn btn-danger btn-xs js-confirm">Отклонить рассказ</a>
-                            @elseif($user['writing']['status'] == 3)
-                            <span class="label label-danger">Рассказ отклонен</span><br><br>
-                            <a href="{{ URL::route('moderator.participants.status', array($user['id'], $user['writing']['id'], 1)) }}" class="btn btn-success btn-xs js-confirm">Одобрить рассказ</a>
-                            @endif
-                        @endif
-                        </td>
-                        <td>
+                            <hr>
                             {{ Form::model($user,array('route'=>array('moderator.participants.save',$user['id']),'method'=>'post')) }}
                             {{ Form::checkbox('winner') }} Победитель <br>
-                            {{ Form::text('number_week', ($user['number_week'] > 0 ? $user['number_week'] : '') ) }} Номер недели <br>
+                            {{ Form::text('number_week', ($user['number_week'] > 0 ? $user['number_week'] : ''), array('style'=>'width:30px;') ) }} Номер недели <br>
 
                             {{ Form::button('Сохранить',array('class'=>'btn btn-success btn-sm','type'=>'submit','style'=>'margin-top:10px;')) }}
                             {{ Form::close() }}
+                        </td>
+                        <td>
+                            <div style="margin-top: 50px">
+                            @if(count($user['writing']) && !empty($user['writing']['writing']))
+                                <p><a target="_blank" href="{{ URL::route('show.participant.writing', $user['writing']['id'].'-'.BaseController::stringTranslite($user['name'].'-'.$user['surname'])) }}">Смотреть рассказ</a></p>
+                            @elseif(count($user['writing']) && empty($user['writing']['writing']))
+                                <p>Рассказ пустой</p>
+                            @else
+                                <p>Рассказ отсутсвует</p>
+                            @endif
+                            </div>
+                            @if(count($user['writing']) && !empty($user['writing']['writing']))
+                                <hr style="margin-bottom: 5px; margin-top: 5px;">
+                                @if($user['writing']['status'] == 2)
+                                    <span class="label label-primary">Ожидает модерации</span><br><br>
+                                    <a href="{{ URL::route('moderator.participants.status', array($user['id'], $user['writing']['id'], 1)) }}" class="btn btn-success btn-xs js-confirm">Одобрить рассказ</a>
+                                    <a href="{{ URL::route('moderator.participants.status', array($user['id'], $user['writing']['id'], 3)) }}" class="btn btn-danger btn-xs js-confirm">Отклонить рассказ</a>
+                                @elseif($user['writing']['status'] == 1)
+                                    <span class="label label-success">Рассказ одобрен</span><br><br>
+                                    <a href="{{ URL::route('moderator.participants.status', array($user['id'], $user['writing']['id'], 3)) }}" class="btn btn-danger btn-xs js-confirm">Отклонить рассказ</a>
+                                @elseif($user['writing']['status'] == 3)
+                                    <span class="label label-danger">Рассказ отклонен</span><br><br>
+                                    <a href="{{ URL::route('moderator.participants.status', array($user['id'], $user['writing']['id'], 1)) }}" class="btn btn-success btn-xs js-confirm">Одобрить рассказ</a>
+                                @endif
+                            @endif
                         </td>
                     </tr>
                 @endforeach
