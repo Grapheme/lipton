f939637d4c94a129be91698cf3515de51b1a8984
Andrew G <voliomas.werdna@gmail.com>
2015-08-24 10:53:46 +0300

    br prizes

--- app/views/templates/site/gallery-prizes.blade.php
+++ app/views/templates/site/gallery-prizes.blade.php
@@ -29,8 +29,8 @@
                     <a href="{{ pageurl('countries') }}" class="prize-link">Lipton Discovery Collection<sup>***</sup></a>
                     <div class="footnote">
                         <p>
-                            <sup>*</sup> Дискавери
-                            <sup>**</sup> Главный приз — сертификат на туристическое путешествие.
+                            <sup>*</sup> Дискавери <br>
+                            <sup>**</sup> Главный приз — сертификат на туристическое путешествие. <br>
                             <sup>***</sup> Дискавери Колекшн
                         </p>
                     </div>
