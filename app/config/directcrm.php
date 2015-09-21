0599668d13c096e25ba0b427fb06dca0cf8eeab9
Владимир <vkharseev@gmail.com>
2015-09-21 11:51:29 +0300

    Логирование

--- app/config/directcrm.php
+++ app/config/directcrm.php
@@ -7,19 +7,15 @@ return array(
         'x_brand' => 'lipton',
         'x_customer_url' => Request::root(),
         'authorization' => array(
-            #'key' => 'D4LrRgsXOEDwWSqlWwej',
-            'key' => 'tFgoNivJYYW11vgSqrXO',
+            'key' => 'D4LrRgsXOEDwWSqlWwej',
             'staffLogin' => 'GrapeLipton2015',
             'staffPassword' => 'ZR2ekMJt',
             'customerId' => '',
             'sessionKey' => ''
         )
     ),
-    #'server_url' => 'https://unilever-services.directcrm.ru',
-    #'host' => 'unilever-services.directcrm.ru',
-
-    'server_url' => 'https://unilever-services.staging.directcrm.ru',
-    'host' => 'unilever-services.staging.directcrm.ru',
+    'server_url' => 'https://unilever-services.directcrm.ru',
+    'host' => 'unilever-services.directcrm.ru',
 
     'send_local_messages' => FALSE,
 
